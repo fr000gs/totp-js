160a5174353228099c43ee8a073c2b05a36830aa
Fat Frog <f47fr0g@gmail.com>
2024-04-21 18:44:54 +0530

    a

--- index.js
+++ index.js
@@ -5,40 +5,16 @@
  * @author   wuyanxin <https://wuyanxin.com>
  * @license  MIT
  */
-/*
-var HOTP;
-var TOTP;
-//var HOTP = require('./lib/hotp.js')
 
-define(['require', './lib/hotp.js', './lib/totp.js'], function (require) {
-  var HOTP = require('./lib/hotp.js');
-  var TOTP = require('./lib/totp.js');
-});
-//var TOTP = require('./lib/totp.js')
-
-TOTP.HOTP = HOTP;
-
-*/
-//module.exports = TOTP;
-/*
-function makepss() {
-  var text = sha512(document.getElementById("psw").value +
-    document.getElementById("usr").value);
-  var value = '';
-  for (var i = 0; i < text.length; i++) {
-    if (i % 8 == 0) {
-      value = value + text[i];
-    }
-  }
-  document.getElementById("pswd").innerHTML = value + "@A";
-}
-*/
+var $secret = document.getElementById('input_secret');
+var $totp = document.getElementById('out_totp');
+var $ttl = document.getElementById('ttl');
 
 var b32_regex = /^[A-Z2-7]+=*$/;
 var T = Math.floor((Date.now() / 1000 - t0) / timeStep);
 
 function makepss() {
-  var secre = document.getElementById("input_secret").value;
+  var secre = $secret.value;
   if (secre.length % 8 === 0 &&
       b32_regex.exec(secre)) {
       var totp = new TOTP(secre);
@@ -47,15 +23,31 @@ function makepss() {
       alert("Secret invalid");
   }
   var code = totp.genOTP();
-  document.getElementById("out_totp").innerHTML = code;
+  $totp.innerHTML = code;
 }
 
+function startInterval() {
+  setInterval(function () {
+    var ttl = Math.floor(Date.now() / 1000 % 30);
+    $ttl.innerHTML = 30 - ttl;
+    if (ttl === 0) {
+      refreshCode();
+    }
+  }, 1000);
+}
+
+function sync2NextSecond() {
+  var ms2NextSecond = 1000 - (Date.now() % 1000);
+  setTimeout(startInterval, ms2NextSecond);
+}
+
+sync2NextSecond();
 
 document.getElementById("makepsbtn")
   .addEventListener("click", makepss, false);
 
 function copyPassword() {
-  var copyText = document.getElementById("pswd").innerHTML;
+  var copyText = $totp.innerHTML;
   navigator.clipboard.writeText(copyText);
 }
 
