b34234e920407b821b79da6f3c750af5472a8b4c
Fat Frog <f47fr0g@gmail.com>
2024-04-23 18:14:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ var $totp = document.getElementById('out_totp');
 var $ttl = document.getElementById('ttl');
 
 var b32_regex = /^[A-Z2-7]+=*$/;
-//var T = Math.floor((Date.now() / 1000 - 30) / timeStep);
 
 function btmakepss() {
-  var secre = $secret.value;
   if (secre.length % 8 === 0 &&
       b32_regex.exec(secre)) {
-      var totp = new TOTP(secre);
   }
   else {
       alert("Secret invalid");
@@ -29,34 +26,11 @@ function btmakepss() {
 }
 
 function makepss() {
+  var secre = $secret.value;
+  var totp = new TOTP(secre);
   var code = totp.genOTP();
   $totp.innerHTML = code;
 }
-
-/*
-function startInterval() {
-  setInterval(function () {
-    var ttl = Math.floor(Date.now() / 1000 % 30);
-    $ttl.innerHTML = 30 - ttl;
-    if (ttl === 0) {
-      makepss();
-    }
-  sync2NextSecond();
-  }, 1000);
-  
-}
-
-
-
-function sync2NextSecond() {
-  var ms2NextSecond = 1000 - (Date.now() % 1000);
-  setTimeout(makettl, 3); // ms2NextSecond);
-}
-
-sync2NextSecond();
-*/
-
-
 
 function makettl() {
     var ttl = Math.floor(Date.now() / 1000 % 30);
@@ -66,18 +40,10 @@ function makettl() {
     }
 }
 
-
-var x = 
-/*
-console.log(x);
-console.log(makettl);
-
-var a = window.setInterval(function() 
-	{alert("a");}, 1000);
-*/
+window.setInterval(makettl, 1000);
 
 document.getElementById("makepsbtn")
-  .addEventListener("click", btmakepss, false);
+  .addEventListener("click", makepss, false);
 
 function copyPassword() {
   var copyText = $totp.innerHTML;
